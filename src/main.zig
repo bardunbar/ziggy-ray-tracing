ac6870b8f10e31aef1d65ecfdb5cc2a40e3cf3e4
bardunbar <61152068+bardunbar@users.noreply.github.com>
2026-06-29 22:09:57 +0100

    Raytracing chapter 3

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,17 @@
 const rl = @import("raylib");
+const math = @import("math.zig");
+
+const Vec3 = math.Vec3;
+const Ray = math.Ray;
+
+fn color(r: Ray) Vec3 {
+    const normalized = r.direction().normalized();
+    const t: f32 = 0.5 * (normalized.y() + 1.0);
+    return Vec3.add(
+        Vec3.multiply_scalar(Vec3.splat(1.0), (1.0 - t)),
+        Vec3.multiply_scalar(Vec3.init(0.5, 0.7, 1.0), t),
+    );
+}
 
 pub fn main() void {
     const screen_width = 800;
@@ -10,15 +23,40 @@ pub fn main() void {
     var screenImage = rl.genImageColor(screen_width, screen_width, rl.Color.black);
     defer rl.unloadImage(screenImage);
 
+    const upper_left_corner = Vec3.init(-2.0, 1.0, -1.0);
+    const horizontal = Vec3.init(4.0, 0.0, 0.0);
+    const vertical = Vec3.init(0.0, -2.0, 0.0);
+    const origin = Vec3.splat(0.0);
+
     // Initialize the screen image for testing
     for (0..screen_height) |y| {
         for (0..screen_width) |x| {
-            const normalized_x = @as(f64, @floatFromInt(x)) / @as(f64, @floatFromInt(screen_width));
-            const normalized_y = @as(f64, @floatFromInt(y)) / @as(f64, @floatFromInt(screen_height));
-            const r = @as(u8, @trunc(normalized_x * 255.99));
-            const g = @as(u8, @trunc(normalized_y * 255.99));
+            const u = @as(f32, @floatFromInt(x)) / @as(f32, @floatFromInt(screen_width));
+            const v = @as(f32, @floatFromInt(y)) / @as(f32, @floatFromInt(screen_height));
 
-            rl.imageDrawPixel(&screenImage, @intCast(x), @intCast(y), rl.Color.init(r, g, 50, 255));
+            const ray = Ray.init(
+                origin,
+                Vec3.add(
+                    upper_left_corner,
+                    Vec3.add(
+                        Vec3.multiply_scalar(horizontal, u),
+                        Vec3.multiply_scalar(vertical, v),
+                    ),
+                ),
+            );
+
+            const col = color(ray);
+
+            const r = @as(u8, @trunc(col.r() * 255.99));
+            const g = @as(u8, @trunc(col.g() * 255.99));
+            const b = @as(u8, @trunc(col.b() * 255.99));
+
+            rl.imageDrawPixel(
+                &screenImage,
+                @intCast(x),
+                @intCast(y),
+                rl.Color.init(r, g, b, 255),
+            );
         }
     }
 
---
+++ src/math.zig
@@ -0,0 +1,142 @@
+pub const Vec3 = struct {
+    data: [3]f32,
+
+    // Investigate the @Vector type, might make some of this easier!
+    pub fn init(d0: f32, d1: f32, d2: f32) @This() {
+        return .{
+            .data = .{ d0, d1, d2 },
+        };
+    }
+
+    pub fn zero() @This() {
+        return .{
+            .data = @splat(0),
+        };
+    }
+
+    pub fn splat(v: f32) @This() {
+        return .{
+            .data = @splat(v),
+        };
+    }
+
+    pub fn x(self: @This()) f32 {
+        return self.data[0];
+    }
+
+    pub fn y(self: @This()) f32 {
+        return self.data[1];
+    }
+
+    pub fn z(self: @This()) f32 {
+        return self.data[2];
+    }
+
+    pub fn r(self: @This()) f32 {
+        return self.data[0];
+    }
+
+    pub fn g(self: @This()) f32 {
+        return self.data[1];
+    }
+
+    pub fn b(self: @This()) f32 {
+        return self.data[2];
+    }
+
+    pub fn add(lhs: @This(), rhs: @This()) @This() {
+        return .{ .data = .{
+            lhs.data[0] + rhs.data[0],
+            lhs.data[1] + rhs.data[1],
+            lhs.data[2] + rhs.data[2],
+        } };
+    }
+
+    pub fn subtract(lhs: @This(), rhs: @This()) @This() {
+        return .{ .data = .{
+            lhs.data[0] - rhs.data[0],
+            lhs.data[1] - rhs.data[1],
+            lhs.data[2] - rhs.data[2],
+        } };
+    }
+
+    pub fn multiply(lhs: @This(), rhs: @This()) @This() {
+        return .{ .data = .{
+            lhs.data[0] * rhs.data[0],
+            lhs.data[1] * rhs.data[1],
+            lhs.data[2] * rhs.data[2],
+        } };
+    }
+
+    pub fn multiply_scalar(lhs: @This(), rhs: f32) @This() {
+        return .{ .data = .{
+            lhs.data[0] * rhs,
+            lhs.data[1] * rhs,
+            lhs.data[2] * rhs,
+        } };
+    }
+
+    pub fn divide(lhs: @This(), rhs: @This()) @This() {
+        return .{ .data = .{
+            lhs.data[0] / rhs.data[0],
+            lhs.data[1] / rhs.data[1],
+            lhs.data[2] / rhs.data[2],
+        } };
+    }
+
+    pub fn length(self: @This()) f32 {
+        return @sqrt(self.data[0] * self.data[0] + self.data[1] * self.data[1] + self.data[2] * self.data[2]);
+    }
+
+    pub fn length_squared(self: @This()) f32 {
+        return self.data[0] * self.data[0] + self.data[1] * self.data[1] + self.data[2] * self.data[2];
+    }
+
+    pub fn normalize(self: *@This()) void {
+        const k = 1.0 / self.length();
+        self.data[0] *= k;
+        self.data[1] *= k;
+        self.data[2] *= k;
+    }
+
+    pub fn normalized(self: @This()) @This() {
+        const k = 1.0 / self.length();
+        return multiply_scalar(self, k);
+    }
+
+    pub fn dot(lhs: @This(), rhs: @This()) f32 {
+        return lhs.data[0] * rhs.data[0] + lhs.data[1] * rhs.data[1] + lhs.data[2] * rhs.data[2];
+    }
+
+    pub fn cross(lhs: @This(), rhs: @This()) @This() {
+        return .{ .data = .{
+            lhs.data[1] * rhs.data[2] - lhs.data[2] * rhs.data[1],
+            -(lhs.data[0] * rhs.data[2] - lhs.data[2] * rhs.data[0]),
+            lhs.data[0] * rhs.data[1] - lhs.data[1] * rhs.data[0],
+        } };
+    }
+};
+
+pub const Ray = struct {
+    a: Vec3,
+    b: Vec3,
+
+    pub fn init(A: Vec3, B: Vec3) @This() {
+        return .{
+            .a = A,
+            .b = B,
+        };
+    }
+
+    pub fn origin(self: @This()) Vec3 {
+        return self.a;
+    }
+
+    pub fn direction(self: @This()) Vec3 {
+        return self.b;
+    }
+
+    pub fn evaluate(self: @This(), t: f32) Vec3 {
+        return Vec3.add(self.a, Vec3.multiply_scalar(self.b, t));
+    }
+};
